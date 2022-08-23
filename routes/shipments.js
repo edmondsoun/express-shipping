9c525c16219fd0a9d69432725b96b2238153b86b
Edmond Soun <edmond.soun@gmail.com>
2022-08-23 16:19:51 -0700

    added schema and modified route

--- routes/shipments.js
+++ routes/shipments.js
@@ -2,6 +2,9 @@
 
 const express = require("express");
 const router = new express.Router();
+const jsonschema = require("jsonschema");
+const orderSchema = require("../schemas/orderSchema.json");
+const { BadRequestError } = require("../expressError")
 
 const { shipProduct } = require("../shipItApi");
 
@@ -14,10 +17,20 @@ const { shipProduct } = require("../shipItApi");
  */
 
 router.post("/", async function (req, res, next) {
+
+  const result = jsonschema.validate(
+    req.body, orderSchema, { required: true });
+
+  if (!result.valid) {
+    const errs = result.errors.map(err => err.stack);
+    throw new BadRequestError(errs);
+  }
+
   const { productId, name, addr, zip } = req.body;
+
   const shipId = await shipProduct({ productId, name, addr, zip });
   return res.json({ shipped: shipId });
-});
 
+});
 
 module.exports = router;
