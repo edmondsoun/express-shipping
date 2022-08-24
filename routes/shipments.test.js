57090d53724b30178b242f53e14b17f7eb4ffeb9
Edmond Soun <edmond.soun@gmail.com>
2022-08-23 17:27:04 -0700

    added mocks for shipment tests

--- routes/shipments.test.js
+++ routes/shipments.test.js
@@ -1,11 +1,18 @@
 "use strict";
 
+const shipItApi = require("../shipItApi");
+shipItApi.shipProduct = jest.fn();
+
 const request = require("supertest");
 const app = require("../app");
 
 
+
 describe("POST /", function () {
   test("valid", async function () {
+
+    shipItApi.shipProduct.mockReturnValue(5001)
+
     const resp = await request(app).post("/shipments").send({
       productId: 1000,
       name: "Test Tester",
@@ -13,19 +20,24 @@ describe("POST /", function () {
       zip: "12345-6789",
     });
 
-    expect(resp.body).toEqual({ shipped: expect.any(Number) });
+    expect(resp.body).toEqual({ shipped: 5001 });
+
   });
-  
+
   test("invalid", async function () {
+
+    shipItApi.shipProduct.mockReturnValue("10001")
+
     const resp = await request(app).post("/shipments").send({
       productId: "1001",
       name: 123,
       addr: 503,
       zip: 342904,
     });
-    
+
     expect(resp.status).toEqual(400);
-    
+
     expect(resp.body.error).toBeTruthy();
+    
   });
 });
